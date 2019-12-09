7a00224c8dda7ed52eddf7a5b71f1730cbd668dd
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2019-12-09 13:27:53 +0100

    Opdatering til EquipmentClass.cs.

--- FWApp/Model/EquipmentClass.cs
+++ FWApp/Model/EquipmentClass.cs
@@ -8,5 +8,46 @@ namespace FWApp.Assets.Model
 {
     class EquipmentClass
     {
+        private int _iD;
+        private string _name;
+        private string _muscleGroup;
+        private string _description;
+
+        public EquipmentClass(int iD, string name, string musclegroup, string description)
+        {
+            _iD = iD;
+            _name = name;
+            _muscleGroup = musclegroup;
+            _description = description;
+        }
+
+        public int ID
+        {
+            get { return _iD; }
+            set { _iD = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Musclegroup
+        {
+            get { return _muscleGroup; }
+            set { _muscleGroup = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(ID)}: {ID}, {nameof(Name)}: {Name}, {nameof(Musclegroup)}: {Musclegroup}, {nameof(Description)}: {Description}";
+        }
     }
 }

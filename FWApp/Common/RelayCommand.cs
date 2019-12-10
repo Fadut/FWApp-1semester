0898e380a62d879f806cacd5f5e0900fb3daa221
Martin Jørgen Pedersen <mart124h@edu.easj.dk>
2019-12-10 10:10:28 +0100

    RelayCommand Update

--- FWApp/Common/RelayCommand.cs
+++ FWApp/Common/RelayCommand.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FWApp.Common
 {
-    class RelayCommand
+    class RelayCommand : ICommand
     {
+        private Action _skiftSide;
 
+        public RelayCommand(Action skiftSide)
+        {
+            _skiftSide = skiftSide;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
     }
 }

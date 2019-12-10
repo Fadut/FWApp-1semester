241dc91ac9ff7c6d1c8e16af8f3c243586a2f8cb
Martin Jørgen Pedersen <mart124h@edu.easj.dk>
2019-12-10 09:57:58 +0100

    FItnessGuide Relay Command

--- FWApp/Common/FitnessGuide_RC.cs
+++ FWApp/Common/FitnessGuide_RC.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FWApp.ViewModel
+namespace FWApp.Common
 {
-    class ForsideVM
+    class FitnessGuide_RC
     {
 
 

968f42909a229cf22621a386650f7b2652d08f0a
Martin Jørgen Pedersen <mart124h@edu.easj.dk>
2019-12-10 10:49:43 +0100

    Forside ViewModel og RelayCommand Updates

--- FWApp/ViewModel/ForsideVM.cs
+++ FWApp/ViewModel/ForsideVM.cs
@@ -3,13 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FWApp.Common;
 
 namespace FWApp.ViewModel
 {
     class ForsideVM
     {
 
+        #region Instance Fields
 
+        private RelayCommand _skiftSideCommand;
+
+
+        #endregion
+
+        #region Properties
+
+        public RelayCommand SkiftSideCommand
+        {
+            get { return _skiftSideCommand; }
+            set { _skiftSideCommand = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public ForsideVM()
+        {
+            SkiftSideCommand = new RelayCommand(TilFitnessGuide);
+            SkiftSideCommand = new RelayCommand(TilTravlhed);
+            SkiftSideCommand = new RelayCommand(TilForside);
+            
+        }
+
+        #endregion
+
+        #region Metoder
+
+        public void TilFitnessGuide()
+        {
+
+        }
+
+        public void TilTravlhed()
+        {
+
+        }
+
+        public void TilForside()
+        {
+
+        }
+
+
+        #endregion
 
     }
 }
